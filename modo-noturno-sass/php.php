937300b00239fbe92621b96fa1c91105f6a19255
Nicholas Campanelli <56810073+niccampanelli@users.noreply.github.com>
2020-03-24 00:26:00 -0300

    Add files via upload

---
+++ modo-noturno-sass/php.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang = "pt-BR">
+    <head>
+        <meta charset ="UTF-8" />
+        <meta vieport ="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="./style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Baloo+2:400,700&display=swap" rel="stylesheet">
+        <title>Modo Noturno com Sass</title>
+    </head>
+    <input type="checkbox" id="switch">
+<body>
+    <header>
+        <label for="switch" class="textoNormal" id="ativar">Ativar/Desativar o Modo Noturno</h1>
+    </header>
+
+    <div class="content">
+        <h1 class="textoNormal">Eu sou um texto do tipo normal! <br> Eu sou um texto de teste para demonstrar o dark mode feito completamente em <b>Sass</b>.</h1>
+        <input type="textbox" id="campo">
+    </div>
+</body>
+</html>
